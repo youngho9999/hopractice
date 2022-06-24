9db45029a20346354b8562c545857fdf6fd0bb6b
youngho9999 <aplblypt@gmail.com>
2022-06-24 17:12:02 +0900

    Initial commit

---
+++ src/main/java/com/practice/hopractice/HopracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.hopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/hopractice/HopracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.hopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
