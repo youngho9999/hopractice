39f544974e31ea32bb4314a9ffee07dcc2225595
youngho9999 <aplblypt@gmail.com>
2022-06-27 21:37:57 +0900

    롬복추가, 본체에 exclude datasource

--- src/main/java/com/practice/hopractice/HopracticeApplication.java
+++ src/main/java/com/practice/hopractice/HopracticeApplication.java
@@ -2,8 +2,10 @@ package com.practice.hopractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class HopracticeApplication {
 
 	public static void main(String[] args) {
