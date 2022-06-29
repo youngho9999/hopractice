731304551091ff80eef490252c8276eb3bf1df7e
youngho9999 <aplblypt@gmail.com>
2022-06-29 15:08:52 +0900

    등록 기능 생성

---
+++ src/main/java/com/practice/hopractice/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.practice.hopractice.service.posts;
+
+import com.practice.hopractice.domain.posts.PostsRepository;
+import com.practice.hopractice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
