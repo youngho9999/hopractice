d6e1c6cede805cd84862d2a0c06b057710b832e2
youngho9999 <aplblypt@gmail.com>
2022-07-01 16:27:55 +0900

    수정, 조회기능 추가

---
+++ src/main/java/com/practice/hopractice/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.practice.hopractice.web.dto;
+
+import com.practice.hopractice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
