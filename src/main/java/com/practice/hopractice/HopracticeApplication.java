a3fe2e52a6eae8b068b86a9ffd58d744f5999cdb
youngho9999 <aplblypt@gmail.com>
2022-07-04 16:46:28 +0900

    jpa auditing 테스트 추가/ 메인 함수에 jpaauditingenable 추가

--- src/main/java/com/practice/hopractice/HopracticeApplication.java
+++ src/main/java/com/practice/hopractice/HopracticeApplication.java
@@ -3,9 +3,10 @@ package com.practice.hopractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-
+@EnableJpaAuditing
 public class HopracticeApplication {
 
 	public static void main(String[] args) {
