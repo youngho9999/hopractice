89e2e80eb45ed938f8cb25c47ae921735ff69ba4
youngho9999 <aplblypt@gmail.com>
2022-07-04 16:16:30 +0900

    jpa auditing 사용해서 생성시간/수정시간 자동화

--- src/main/java/com/practice/hopractice/domain/posts/Posts.java
+++ src/main/java/com/practice/hopractice/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.practice.hopractice.domain.posts;
 
 
+import com.practice.hopractice.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
