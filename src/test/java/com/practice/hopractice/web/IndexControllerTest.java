713db441e5e9053234a52371386350e3070c8a7c
youngho9999 <aplblypt@gmail.com>
2022-07-04 20:01:37 +0900

    인덱스 html 생성/컨트롤러 테스터 머스테치 파일

---
+++ src/test/java/com/practice/hopractice/web/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package com.practice.hopractice.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //given
+
+        //when
+        String body = this.restTemplate.getForObject("/",String.class);
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
