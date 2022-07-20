922ff062415c7f25c6a042321ab50e93417a09df
youngho9999 <aplblypt@gmail.com>
2022-07-20 17:58:51 +0900

    전체 조회 기능 추가를 위한 변경

--- src/main/java/com/practice/hopractice/service/posts/PostsService.java
+++ src/main/java/com/practice/hopractice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.practice.hopractice.service.posts;
 
 import com.practice.hopractice.domain.posts.Posts;
 import com.practice.hopractice.domain.posts.PostsRepository;
+import com.practice.hopractice.web.dto.PostsListResponseDto;
 import com.practice.hopractice.web.dto.PostsResponseDto;
 import com.practice.hopractice.web.dto.PostsSaveRequestDto;
 import com.practice.hopractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,13 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 
 }
